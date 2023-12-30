7eefd3f4463857b76c8c16878d708aacf2f19a76
John Schimenz <john.schimenz@gmail.com>
2023-12-29 18:06:32 -0600

    ToDoFactory created

---
+++ main.js
@@ -0,0 +1,21 @@
+// BEHIND THE SCENES STUFF
+
+// Project factory
+
+// ToDo factory
+const ToDoFactory = (title, description, dueDate, priority, notes, checklist) => {
+    title = title;
+    description = description;
+    dueDate = dueDate;
+    priority = priority;
+    notes = notes;
+    checklist = checklist;
+    return {title, description, dueDate, priority, notes, checklist};
+}
+
+
+// DOM STUFF
+
+// Function to remove elements in a container
+
+// Function to add elements to a container
